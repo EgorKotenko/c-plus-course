7ea1c246e01466ef2a3082587d8fef48b752e63f
Egor <kotenko.george@gmail.com>
2017-05-26 00:37:06 +0300

    Add files via upload

---
+++ Problems 10/Задача №1/modul_10_1.h
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "stdio.h"
+#include "iostream"
+#include "math.h"
+
+using namespace std;
+
+#pragma once
+
+struct List;
+
+void addToList(List *&input, int k, int p);
+void printPolynomial(List *&input);
+void deleteList(List *&input);
+
